63641d5f57a5da02d2d0dd1ca55b530e2e61258a
BrijeshSrivastav <brijeshsrivastav99@gmail.com>
2023-09-18 00:09:34 +0530

    drawer bottom navigation

---
+++ src/pages/Splash.js
@@ -0,0 +1,18 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import {View, Text} from 'react-native';
+import React, {useEffect} from 'react';
+
+const Splash = ({navigation}) => {
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.navigate('Parent');
+    }, 2000);
+  }, []);
+  return (
+    <View>
+      <Text>Splash</Text>
+    </View>
+  );
+};
+
+export default Splash;
