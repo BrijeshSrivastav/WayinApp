9bd8bfe1c3ed8fceed0cd763246eff7b107e1aff
BrijeshSrivastav <brijeshsrivastav99@gmail.com>
2023-09-20 22:23:10 +0530

    tab icon

--- src/bottom/BottomNavigator.js
+++ src/bottom/BottomNavigator.js
@@ -4,10 +4,18 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Screen1 from './Screen1';
 import Screen2 from './Screen2';
 import Screen3 from './Screen3';
+import Icon from 'react-native-vector-icons/AntDesign';
+
+import Svg from 'react-native-svg';
 const Bottom = createBottomTabNavigator();
 const BottomNavigator = () => {
   return (
-    <Bottom.Navigator>
+    <Bottom.Navigator 
+    
+    screenOptions={{
+      tabBarStyle: { backgroundColor: '#1c5791', paddingBottom: 10,borderTopLeftRadius: 5,borderTopRightRadius:5},
+      tabBarPressColor: '#3C60AA',
+    }}>
       <Bottom.Screen
         name="Screen1"
         component={Screen1}
@@ -15,14 +23,15 @@ const BottomNavigator = () => {
           headerShown: false,
           tabBarIcon: tabInfo => {
             return (
-              <Image
-                source={require('../checkbox.png')}
-                style={{
-                  width: 20,
-                  height: 20,
-                  tintColor: tabInfo.focused ? 'purple' : 'black',
-                }}
-              />
+              <Icon name="home" size={30} color="#900" />
+              // <Icon
+              //   source={require('../checkbox.png')}
+              //   style={{
+              //     width: 20,
+              //     height: 20,
+              //     tintColor: tabInfo.focused ? 'purple' : 'black',
+              //   }}
+              // />
             );
           },
         }}
