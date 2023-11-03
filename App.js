21e9aa9d3803ac8fcd08798e8a6ce115aa34075d
Roly <brijeshsrivastav99@gmail.com>
2023-11-03 21:51:26 +0530

    3NovKD

--- App.js
+++ App.js
@@ -3,17 +3,15 @@ import AppNavigator from './src/AppNavigator';
 import store from "./src/redux/store";
 import { Provider } from "react-redux";
 import { LogBox } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
-LogBox.ignoreLogs(['Warning: ...']); // Ignore log notification by message
+LogBox.ignoreLogs(['Warning: ...']);  // Ignore log notification by message
 LogBox.ignoreAllLogs()
 
 const App = () => {
-
   return (
   <Provider store={store}>
-  <AppNavigator />
- </Provider>
+    <AppNavigator />
+  </Provider>
   );
 };
 
